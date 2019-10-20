3369b7dedced8e264e9b1125839cf77a6572bf7b
Harsha Shekar <harshashekar.1987@gmail.com>
2019-10-20 11:27:29 +0530

    Added new problem set for TA

---
+++ whatflix/src/main/java/com/shekar/geektrust/WhatflixApplication.java
@@ -0,0 +1,13 @@
+package com.shekar.geektrust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatflixApplication.class, args);
+	}
+
+}
---
+++ whatflix/src/test/java/com/shekar/geektrust/WhatflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shekar.geektrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
